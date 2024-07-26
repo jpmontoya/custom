5e923a4f6cc74f187aecd5b7a14a59feb78b217c
jpmontoya <74821510+jpmontoya@users.noreply.github.com>
2024-07-26 00:22:46 +0000

    Se añade audio y background

--- custom.sh
+++ custom.sh
@@ -3,4 +3,8 @@
 #Cambiar el idioma del teclado
 setxkbmap latam
 
+#Cambiar el audio de salida
+pactl set-sink-port 4 analog-output-lineout
+
 #Cambiar el background
+gsettings set org.mate.background picture-filename "$(pwd)/wallpaper.jpg"
