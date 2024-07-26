9f5297b6f47a0226bf47713957929360b470ce83
jpmontoya <74821510+jpmontoya@users.noreply.github.com>
2024-07-26 02:14:31 +0000

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -9,4 +9,4 @@ pactl set-sink-port 4 analog-output-lineout
 #Cambiar el background
 gsettings set org.mate.background picture-filename "$(pwd)/wallpaper.jpg"
 
-# https://github.com/daniruiz/skeuos-gtk.git theme
+# https://github.com/daniruiz/skeuos-gtk.git https://www.mate-look.org/p/1441725 theme
